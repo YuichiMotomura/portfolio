880e3b50c643d073e499791b551a246ab346d8a4
YuichiMotomura <yu1liveinwellisle@gmail.com>
2019-10-22 20:00:03 +0900

    Update spots_controller.rb
    spotsコントローラに、new, createsのメソッドを作成

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,5 +1,27 @@
 class SpotsController < ApplicationController
   def index
-    # binding.pry
+  end
+
+  def new
+    @spot = Spot.new
+    @spot.photos.build
+    @spot.build_radar
+  end
+
+  def create
+    spot = Spot.new(spot_params)
+    
+    if spot.save
+      redirect_to root_path, notice: '投稿が完了しました'
+    else
+        render :new
+    end
+    
+  end
+
+
+  private
+  def spot_params
+    params.require(:spot).permit(:title, :text, :prefecture_id, photos_attributes: [:image], radar_attributes: [:retro, :rare, :insta, :emotional, :delicious, :kawaii]).merge(author_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'spots#index'
+  resources :spots
   
 end
