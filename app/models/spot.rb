64f89aea1b660396c57df9d09f11b75b48b085a0
YuichiMotomura <yu1liveinwellisle@gmail.com>
2019-10-28 19:50:55 +0900

    Update models
    userとspotにアソシエーションを組んだ
    radarのvalidation終生

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,7 +2,8 @@ class Spot < ApplicationRecord
   has_many :photos
   has_one :radar
   accepts_nested_attributes_for :photos, :radar
-  belongs_to :prefecture, optional: true
+  belongs_to :prefecture
+  belongs_to :author, class_name: 'User'
 
   validates :title, :text, :prefecture_id, presence: true
   validates :text, length: {maximum: 400}
