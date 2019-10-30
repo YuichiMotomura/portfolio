9fc382fc9ea51c3d10edfb2ba7b471b9586f234a
YuichiMotomura <yu1liveinwellisle@gmail.com>
2019-10-30 19:55:10 +0900

    add column to prefecture
    prefectureテーブルに、都道府県と市町村の親子関係を記述するancestryカラムを追加

--- db/migrate/20191019041927_create_photos.rb
+++ db/migrate/20191019041927_create_photos.rb
@@ -1,11 +1,9 @@
-class CreatePhotos < ActiveRecord::Migration[5.2]
-    def change
-      create_table :photos do |t|
-        t.string :image, null: false
-        t.references :spot, index: true, foreign_key: true
-        t.timestamps
-      end
-    end
+class CreatePhotos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :photos do |t|
+      t.string :image, null: false
+      t.references :spot, foreign_key: true
+      t.timestamps
+    end
   end
-  
-  
+end
