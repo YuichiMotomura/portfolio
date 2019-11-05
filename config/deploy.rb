ea88115c44c56100aa8acfe26cf002ff0ec01eeb
YuichiMotomura <yu1liveinwellisle@gmail.com>
2019-11-05 21:47:40 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ set :default_env, {
    path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH" ,
    AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
    AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"],
-   MAP_API: <%= ENV["GOOGLE_MAP"] %>
+   MAP_API: ENV["GOOGLE_MAP"]
   }
 
 # Default value for local_user is ENV['USER']
